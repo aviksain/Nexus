8842a5c56d1a04bf4d37c7f4c4cb1300bfc11d44
avik <aviksain007@gmail.com>
2024-07-10 16:34:07 +0530

    change the file name while uploading using multer

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,11 +1,16 @@
 import multer from "multer";
+import crypto from "crypto";
+import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    const randomString = crypto.randomBytes(16).toString("hex");
+    const fileExtension = path.extname(file.originalname);
+    const randomFileName = `${randomString}${fileExtension}`;
+    cb(null, randomFileName);
   },
 });
 
